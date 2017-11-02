16b4a0547a121e0f780e75c644771cc0c5340df6
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2017-11-02 14:56:07 -0200

    versão melhorada do talude

--- DesenhoTalude/myscript.js
+++ DesenhoTalude/myscript.js
@@ -2,53 +2,71 @@
 
 function drawSlope(angle,height) {
     /*
-               D    C
+               D        C
                |->(height)
     F     E-----  
             disp
-    A               B
+    A                   B
     */
     //fatores multiplicativos do talude
-    var factor1 = 2;
-    var factor2 = 2;
-    var factor3 = 2;
+    var factor1 = 2; //fator que gera a altura de B até C
+    var factor2 = 2; //fator que gera o comprimento de F até E
+    var factor3 = 2; //fator que gera o comprimento de D até C
 
 
     var baseX = 0;
     //fazendo assim pois o P(0,0) fica no canto superior esquerdo
-    var baseY = renderer.view.height;
+    var baseY = renderer.view.height; //Y base é agora o canto inferior esquerdo
     var slope = new PIXI.Graphics();
     slope.lineStyle(4,0xFFFFFF,1);
-    slope.x=20;
-    slope.y=-50;
-    slope.beginFill(0x00FF00);
-    var A = {x:baseX,y:baseY}
-    var displacement = height/Math.tan(Math.PI*angle/180);
-    console.log(displacement);
-    var B = {x:4*height+displacement,
-             y:baseY}
-    var C = {x:B.x,y:baseY-factor1*height}
-    var D = {x:C.x-2*height,y:C.y}
-    var E = {x:2*height,y:baseY-(factor1*height-height)}
-    var F = {x:0,y:E.y}    
-
+    slope.x=20;//movendo o slope pra fora do canto
+    slope.y=-20;
+    var displacement = height/Math.tan(Math.PI*angle/180);    
+    var A = {
+        x:baseX,
+        y:baseY
+    }    
+    var B = {
+        x:factor2*factor3*height+displacement,
+        y:baseY
+            }
+    var C = {
+        x:B.x,
+        y:baseY-factor1*height
+    }
+    var D = {
+        x:C.x-factor2*height,
+        y:C.y
+    }
+    var E = {
+        x:factor2*height,
+        y:baseY-(factor1*height-height)
+    }
+    var F = {
+        x:0,
+        y:E.y
+    }    
+    //lista que liga todos os pontos do talude
     var path = [
         A.x,A.y,
         B.x,B.y,
         C.x,C.y,
         D.x,D.y,
         E.x,E.y,
-        F.x,F.y
+        F.x,F.y,
+        A.x,A.y
     ]
+    slope.beginFill(0x00FF00);
     slope.drawPolygon(path);
     slope.endFill();
     
+    //acessando variavel global, considerar receber por parametro
     stage.addChild(slope);
 
 }
 
-
-var renderer = PIXI.autoDetectRenderer(512, 512,
+// pro render só precisa passar o width e o height o resto é opcional
+var renderer = PIXI.autoDetectRenderer(512, 256,
 {antialias: true, transparent: false, resolution: 1}
 );
 renderer.backgroundColor = 0x061639;
@@ -60,17 +78,7 @@ document.body.appendChild(renderer.view);
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
 
-// PIXI.loader.add("Lenna.png").load(setup);
-
-// //This `setup` function will run when the image has loaded
-// function setup() {
-
-// //Create the `cat` sprite from the texture
-// var lenna = new PIXI.Sprite(
-//     PIXI.loader.resources["Lenna.png"].texture
-// );
-// stage.addChild(lenna);
-//}
+//drawSlope(angulo,altura)
 drawSlope(30,80);
 
 
