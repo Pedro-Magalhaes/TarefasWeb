5a069d5c23b2cfde786d0424791bd03ab5f3d85d
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2017-11-03 21:12:35 -0200

    agora o cursor reconhece qual layer ele está sobre

--- DesenhoLayers/myscript.js
+++ DesenhoLayers/myscript.js
@@ -11,12 +11,11 @@ function drawLayers(heights,inicialPoints,colors,totalHeight,angle,mystage) {
     }
     var MARGIN = 0.01; //margem de erro pra comparação de float
 
-    var alturaRestante = totalHeight;
-    var myLayer = new PIXI.Graphics();
-    myLayer.lineStyle(2,0xFFFFFF,1);
+    var alturaRestante = totalHeight;   
 
     for (var i = 0; i < heights.length; i++) { 
-         
+        var myLayer = new PIXI.Graphics();
+        myLayer.lineStyle(2,0xFFFFFF,1);
         if ((alturaRestante+MARGIN)-heights[i] < 0){
             //raise error (como fazer?)
             console.log("Erro de Altura, nao vou desenhar o layer",i,"e seguintes",heights,totalHeight);
@@ -46,25 +45,36 @@ function drawLayers(heights,inicialPoints,colors,totalHeight,angle,mystage) {
         inicialPoints[0]=layerD;
         inicialPoints[1]=layerC;
 
-        var layerPath = [
-            layerA.x,layerA.y,
-            layerB.x,layerB.y,
-            layerC.x,layerC.y,
-            layerD.x,layerD.y,
-            layerA.x,layerA.y,
-        ]
+        var layerPath = new PIXI.Polygon(
+            new PIXI.Point (layerA.x,layerA.y),
+            new PIXI.Point (layerB.x,layerB.y),
+            new PIXI.Point (layerC.x,layerC.y),
+            new PIXI.Point (layerD.x,layerD.y),
+            new PIXI.Point (layerA.x,layerA.y),
+        )
+        myLayer.alpha = 0.8; //só pra destacar
         myLayer.beginFill(colors[i]);
         myLayer.drawPolygon(layerPath);
         myLayer.endFill();
-        
+        myLayer.hitArea = layerPath;
+        myLayer.interactive = true;
+        myLayer.buttonMode = true;
+
+        myLayer.mouseover = function (mouseData) {
+            this.alpha = 1;
+        };
+        myLayer.mouseout = function (mouseData) {
+            this.alpha = .8;
+        };
+        mystage.addChild(myLayer);
     }
 
-//como alterei as coordenadas do slope depois de desenhar, tive que fazer o mesmo aqui
+    //como alterei as coordenadas do slope depois de desenhar, tive que fazer o mesmo aqui
 
-
-    mystage.addChild(myLayer);
+    
+    
 }
-function drawSlope(angle,height) {
+function drawSlope(angle,height,stage) {
     /*
                D        C
                |->(height)
@@ -80,9 +90,12 @@ function drawSlope(angle,height) {
 
     var baseX = 0;
     //fazendo assim pois o P(0,0) fica no canto superior esquerdo
-    var baseY = renderer.view.height; //Y base é agora o canto inferior esquerdo
+    var baseY = app.renderer.view.height; //Y base é agora o canto inferior esquerdo
     var slope = new PIXI.Graphics();
+    //slope.interactive=true;
+    //slope.buttonMode=true;
     slope.lineStyle(2,0xFFFFFF,1);
+    
     
     var displacement = height/Math.tan(Math.PI*angle/180);    
     var A = {
@@ -121,20 +134,20 @@ function drawSlope(angle,height) {
     ]
     slope.beginFill(0x00FFF0);
     slope.drawPolygon(path);
-    slope.endFill();
+    slope.endFill();    
 
-//acessando variavel global stage, considerar receber por parametro
+    //acessando variavel global stage, considerar receber por parametro
     stage.addChild(slope);
 
-/* arrumando as variaveis pra chamar o drawlayers */
+    /* arrumando as variaveis pra chamar o drawlayers */
     C.y=E.y //mutreta pra nao ter que fazer outro ponto
     var inicial = [
         E,C
     ]
     var colors = [
-        0xff0000,
-        0x00ff00,
-        0x0000ff,
+        0xfff002,
+        0xf00f00,
+        0xf0b5ff,
     ]
     var alturas = [
         height/3,
@@ -147,29 +160,21 @@ function drawSlope(angle,height) {
  
 }
 
+var width = document.getElementById('canvas').clientWidth;
+var height = (9*width)/16; //16/9 aspect ratio
+
 // pro render só precisa passar o width e o height o resto é opcional
-var renderer = PIXI.autoDetectRenderer(512, 256,
-{antialias: true, transparent: false, resolution: 1}
-);
-renderer.backgroundColor = 0x061639;
-renderer.autoResize = true;
+var app = new PIXI.Application(width, height,{backgroundColor : 0xffffff},{antialias:true},{transparent:true});
+//app.stage.position.set(width1/2, height2/2);
 
 //Add the canvas to the HTML document
-document.body.appendChild(renderer.view);
+document.getElementById('canvas').appendChild(app.view);
 
 //Create a container object called the `stage`
-var stage = new PIXI.Container();
+//var stage = new PIXI.Container();
 
 //drawSlope(angulo,altura)
-drawSlope(30,80);
+drawSlope(30,80,app.stage);
 
-//andando com o desenho
-stage.x+=15;
-stage.y-=15;
-
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
-
-    
-          
-        
+   
+       
